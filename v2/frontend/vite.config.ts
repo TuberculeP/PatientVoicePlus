1c73b796df6d3636861827280935c61dcb3783f2
ImDimeh <porcepic91@gmail.com>
2026-05-20 10:13:18 +0200

    ajout du fichier sitemap.xml au build du site

--- v2/frontend/vite.config.ts
+++ v2/frontend/vite.config.ts
@@ -2,9 +2,12 @@ import { fileURLToPath, URL } from 'node:url'
 import tailwindcss from '@tailwindcss/vite'
 import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
+import sitemap from 'vite-plugin-sitemap'
 
 export default defineConfig({
-  plugins: [vue(), tailwindcss()],
+  plugins: [vue(), tailwindcss() ,sitemap({
+      hostname: 'https://patient-voice.felix-lavieville.fr/'
+    })],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
