c41a0c900bb748ab5926a4badfbc240e44f143a5
grmvincent <vincentgerme478@gmail.com>
2026-05-20 15:08:09 +0200

    feat: add admin module and authentication routes, update environment variables

--- v2/frontend/src/App.vue
+++ v2/frontend/src/App.vue
@@ -1,14 +1,6 @@
 <script setup lang="ts">
-import TheFooter from './views/TheFooter.vue'
-import TheHeader from './views/TheHeader.vue'
 </script>
 
 <template>
-  <div class="min-h-screen flex flex-col bg-gray-50 text-gray-900">
-    <TheHeader />
-    <main class="flex-1">
-      <RouterView />
-    </main>
-    <TheFooter />
-  </div>
+  <RouterView />
 </template>
